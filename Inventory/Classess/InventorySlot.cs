8b529940032f361aa3e50ad9ab4d6fa83b993858
Rezistencee <86205500+Rezistencee@users.noreply.github.com>
2023-08-26 21:29:07 +0300

    InventorySlot.cs base realization for future architecture.

--- Inventory/Classess/InventorySlot.cs
+++ Inventory/Classess/InventorySlot.cs
@@ -5,19 +5,37 @@ namespace Inventory.Classess
     //TODO: Consider as a replacement for stacking
     public class InventorySlot
     {
+        private static int _id = 1;
         private IItem _item;
         private int _quantity;
+        private int _stackableLimit;
+
+        public IItem Item
+        {
+            get
+            {
+                return _item;
+            }
+        }
 
         public InventorySlot()
         {
             _item = null;
             _quantity = 0;
+            _id = _id + 1;
+            _stackableLimit = 1;
         }
         
         public InventorySlot(IItem item, int quantity)
         {
             _item = item;
             _quantity = quantity;
+            _id = _id + 1;
+
+            if (item is IConsumables consumablesItem)
+                _stackableLimit = consumablesItem.StackableLimit;
+            else
+                _stackableLimit = 1;
         }
     }
 }
